0bad55f16287f32172d930f4f498212341a20b3f
DarkHorse0311 <dark.horse.dev0311@gmail.com>
2018-12-15 23:53:05 -0500

    setup server boilerplate for REST api with knex

---
+++ server/knexfile.js
@@ -0,0 +1,50 @@
+// const localPgConnection = {
+//   host: 'localhost',
+//   database: 'keep',
+//   user:     'username',
+//   password: 'password'
+// }
+
+// const dbConnection = process.env.DATABASE_URL || localPgConnection;
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/dev.sqlite3'
+    },
+    defaultToNull : true,
+    migrations: { directory: './database/migrations'},
+    seeds: { directory: './database/seeds'},
+  },
+
+
+  // production: {
+  //   client: 'pg',
+  //   connection: dbConnection,
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: { directory: './database/migrations'},
+  //   seeds: { directory: './database/seeds'},
+  // }
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+};
