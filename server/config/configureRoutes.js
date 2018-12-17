944eace450369e7c403ba8cfc82dfeb3f5b91cf4
DarkHorse0311 <dark.horse.dev0311@gmail.com>
2018-12-16 20:18:53 -0500

    added route to get all resturant names

--- server/config/configureRoutes.js
+++ server/config/configureRoutes.js
@@ -1,5 +1,9 @@
+const restaurantsRouter = require('../routes/restaurantsRouter');
+
 module.exports = server => {
     server.get('/', (req, res) => {
         res.status(200).json({server: 'alive'})
     })
+
+    server.use('/api/res', restaurantsRouter);
 }
---
+++ server/routes/restaurantsRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const db = require('../database/dbConfig');
+
+router.get('/names', async (req, res) => {
+
+    const names = await db('restaurants').column('name');
+    console.log('names', names);
+    res.status(200).json({names});
+});
+
+module.exports = router;
