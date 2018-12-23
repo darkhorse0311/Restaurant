b40f2375aa8e05f8413c9f0c8302b2338c1509fd
DarkHorse0311 <dark.horse.dev0311@gmail.com>
2018-12-23 14:22:20 -0500

    updated to react-mapbox-gl library

--- client/src/Components/MapContainer.js
+++ client/src/Components/MapContainer.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-import mapboxgl from 'mapbox-gl'
 
-const mapBoxToken = "pk.eyJ1Ijoicm5sZCIsImEiOiJjanBzdGdjODkxb3JwM3ltZm1vOWQxOGFqIn0._OcNwJ_Q7XvH6a0hEahVnQ";
+import ReactMapBoxGl, { Layer, Feature, Popup } from 'react-mapbox-gl';
 
-mapboxgl.accessToken = mapBoxToken;
+const token= "pk.eyJ1Ijoicm5sZCIsImEiOiJjanBzdGdjODkxb3JwM3ltZm1vOWQxOGFqIn0._OcNwJ_Q7XvH6a0hEahVnQ";
+
+const Mapbox = ReactMapBoxGl({
+    minZoom: 8,
+    maxZoom: 15,
+    accessToken: token
+  });
+
+const mapStyle = {
+flex: 1,
+width: '100vw',
+height: '100vh'
+};
 
 // mapbox://styles/mapbox/satellite-v9
 // mapbox://styles/mapbox/dark-v9
@@ -11,44 +22,21 @@ mapboxgl.accessToken = mapBoxToken;
 class MapContainer extends Component {
 
     state = {
-        lng: -74.0060,
-        lat: 40.7128,
-        zoom: 12
+        fitBounds: undefined,
+        center: [-74.0060, 40.7128],
+        zoom: [12],
+        station: undefined,
+        stations: {}
     }
-
-    componentDidMount() {
-        const { lng, lat, zoom } = this.state;
-    
-        const map = new mapboxgl.Map({
-          container: this.mapContainer,
-          style: 'mapbox://styles/mapbox/dark-v9',
-          center: [lng, lat],
-          zoom
-        });
-    
-        map.on('move', () => {
-          const { lng, lat } = map.getCenter();
-    
-          this.setState({
-            lng: lng.toFixed(4),
-            lat: lat.toFixed(4),
-            zoom: map.getZoom().toFixed(2)
-          });
-        });
-      }
 
     render() {
 
         return (
-            <div 
-                ref={el => this.mapContainer = el} 
-                style={{
-                    width: '100vw',
-                    height: '100vh',
-                    position: 'fixed',
-                    top: 0,
-                    left: 0
-                }}
+            <Mapbox 
+                // eslint-disable-next-line react/style-prop-object
+                style="mapbox://styles/mapbox/dark-v9"
+                containerStyle={mapStyle}
+
             />
         );
     }
