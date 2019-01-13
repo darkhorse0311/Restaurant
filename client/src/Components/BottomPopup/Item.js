8abd5f994f8c56b6368f2f0256dc8e2cef673e79
DarkHorse0311 <dark.horse.dev0311@gmail.com>
2019-01-12 23:59:46 -0500

    cleaned up code, seperated mock data into seperate files, broke down BottomPopup component

---
+++ client/src/Components/BottomPopup/Item.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Item = ({item}) => {
+    return (
+        <StyledItem>
+            <h3>{item.name}</h3>
+            <ItemMacros>
+                <Macro>
+                    <Gram>
+                        {item.carbs}
+                    </Gram>
+                    <MacroTitle>
+                        Carbs
+                    </MacroTitle>
+                </Macro>
+                <Macro>
+                    <Gram>
+                        {item.protein}
+                    </Gram>
+                    <MacroTitle>
+                        Protein
+                    </MacroTitle>
+                </Macro>
+                <Macro>
+                    <Gram>
+                        {item.fats}
+                    </Gram>
+                    <MacroTitle>
+                        Fats
+                    </MacroTitle>
+                </Macro>
+            </ItemMacros>
+        </StyledItem>
+    );
+}
+
+export default Item;
+
+const MacroTitle = styled.span`
+    font-size: 12px;
+    font-weight: 600;
+    text-transform: uppercase;
+    text-align: center;
+`;
+
+const Gram = styled.span`
+    font-size: 22px;
+    font-weight: 800;
+`;
+
+const Macro = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const ItemMacros = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    width: 100%;
+`;
+
+const StyledItem = styled.div`
+    width: 200px;
+    min-width: 200px;
+    height: 100%;
+    border: 1px solid rgba(0,0,0,0.4);
+    border-radius: 12px;
+    margin-right: 10px;
+    position: relative;
+    padding: 10px 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+
+    h3 {
+        margin: 0px;
+    }
+`;
--- client/src/Components/BottomPopup/index.js
+++ client/src/Components/BottomPopup/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
+import Item from './Item';
 
 const BottomPopup = ({place, clearPlace}) => {
     return (
@@ -11,35 +12,7 @@ const BottomPopup = ({place, clearPlace}) => {
             <ItemList>
             {
                 place.items.map((item, i) => (
-                    <Item key={i}>
-                        <h3>{item.name}</h3>
-                        <ItemMacros>
-                            <Macro>
-                                <Gram>
-                                    {item.carbs}
-                                </Gram>
-                                <MacroTitle>
-                                    Carbs
-                                </MacroTitle>
-                            </Macro>
-                            <Macro>
-                                <Gram>
-                                    {item.protein}
-                                </Gram>
-                                <MacroTitle>
-                                    Protein
-                                </MacroTitle>
-                            </Macro>
-                            <Macro>
-                                <Gram>
-                                    {item.fats}
-                                </Gram>
-                                <MacroTitle>
-                                    Fats
-                                </MacroTitle>
-                            </Macro>
-                        </ItemMacros>
-                    </Item>
+                    <Item key={i} item={item}/>
                 ))
             }
             </ItemList>
@@ -48,50 +21,6 @@ const BottomPopup = ({place, clearPlace}) => {
 }
 
 export default BottomPopup;
-
-const MacroTitle = styled.span`
-    font-size: 12px;
-    font-weight: 600;
-    text-transform: uppercase;
-    text-align: center;
-`;
-
-const Gram = styled.span`
-    font-size: 22px;
-    font-weight: 800;
-`;
-
-const Macro = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`;
-
-const ItemMacros = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-start;
-    width: 100%;
-`;
-
-const Item = styled.div`
-    width: 200px;
-    min-width: 200px;
-    height: 100%;
-    border: 1px solid rgba(0,0,0,0.4);
-    border-radius: 12px;
-    margin-right: 10px;
-    position: relative;
-    padding: 10px 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-start;
-
-    h3 {
-        margin: 0px;
-    }
-`;
 
 const ItemList = styled.div`
     height: 165px;
