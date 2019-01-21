dce12f098330b3c0330ec4b8c088fc2de088fe8f
DarkHorse0311 <dark.horse.dev0311@gmail.com>
2019-01-20 21:56:18 -0500

    added left arrow component to Arrows component

--- client/src/Components/BottomPopup/Arrows.js
+++ client/src/Components/BottomPopup/Arrows.js
@@ -3,9 +3,14 @@ import styled from 'styled-components'
 
 const Arrows = () => {
     return (
+        <>
+        <LeftArrow>
+        <i className="fas fa-angle-left"></i>
+        </LeftArrow>
         <RightArrow>
             <i className="fas fa-angle-right"></i>
         </RightArrow>
+        </>
     );
 }
 
@@ -16,7 +21,23 @@ const RightArrow = styled.span`
     right: 0;
     top: 0;
     height: 100%;
-    width: 100px;
+    width: 40px;
+    border-top-right-radius: 12px;
+    z-index: 2;
+    background-color: white;
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 40px;
+`;
+
+const LeftArrow = styled.span`
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 40px;
     border-top-right-radius: 12px;
     z-index: 2;
     background-color: white;
