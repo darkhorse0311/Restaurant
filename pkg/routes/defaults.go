1a355ac5d3a1556020b2ad4522c9c4d0080540ab
DarkHorse0311 <dark.horse.dev0311@gmail.com>
2019-03-24 19:50:14 -0400

    added testing for default routes

--- pkg/routes/defaults.go
+++ pkg/routes/defaults.go
@@ -1,29 +1,17 @@
 package routes
 
 import (
-	"encoding/json"
 	"net/http"
-	"os"
 )
 
 // getServerIsUp '/' endpoint cheks if server is up
 func getServerIsUp(w http.ResponseWriter, req *http.Request) {
-	port := os.Getenv("PORT")
 
-	res, _ := json.Marshal(struct {
-		Status  int    `json:"status"`
-		Message string `json:"message"`
-	}{Status: 200, Message: "server live on port: " + port})
-
-	w.Write(res)
+	w.Write([]byte("server is live"))
 }
 
 // routeNotFound '/*' endpoint for undefined routes
 func routeNotFound(w http.ResponseWriter, req *http.Request) {
-	res, _ := json.Marshal(struct {
-		Status  int    `json:"status"`
-		Message string `json:"message"`
-	}{Status: 404, Message: "route not found"})
-
-	w.Write(res)
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("route not found"))
 }
